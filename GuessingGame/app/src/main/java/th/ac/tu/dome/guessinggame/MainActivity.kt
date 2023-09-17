ef276c7e65d535dddec49cfb6477deb8211fb1b8
JJSenior03 <sathit.jun03@gmail.com>
2023-09-17 15:19:00 +0700

    implement count function and edit layouts

--- GuessingGame/app/src/main/java/th/ac/tu/dome/guessinggame/MainActivity.kt
+++ GuessingGame/app/src/main/java/th/ac/tu/dome/guessinggame/MainActivity.kt
@@ -3,6 +3,8 @@ package th.ac.tu.dome.guessinggame
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
@@ -12,12 +14,14 @@ import androidx.appcompat.widget.Toolbar
 class MainActivity : AppCompatActivity() {
 
     lateinit var textView: TextView
+    lateinit var countTextView: TextView
     lateinit var editText: EditText
-    lateinit var imageButtontRefresh: ImageButton
-    lateinit var  imageButtonCheck: ImageButton
+    lateinit var buttonPlayAgain: Button
+    lateinit var imageButtonCheck: ImageButton
     lateinit var hintTextView: TextView
 
     var random: Int = nextInt(1,100)
+    var count: Int = 0
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,38 +33,52 @@ class MainActivity : AppCompatActivity() {
 
 
         textView = findViewById(R.id.textView)
+        countTextView = findViewById(R.id.countTextView)
         editText = findViewById(R.id.editText)
-        imageButtontRefresh = findViewById(R.id.imageButtonRefresh)
+        buttonPlayAgain = findViewById(R.id.buttonPlayAgain)
         imageButtonCheck = findViewById(R.id.imageButtonCheck)
         hintTextView = findViewById(R.id.hintTextView)
 
         textView.text = "Try to guess the number i'm thinking of from 1-100"
+        countTextView.text = "Guesses: $count"
 
         imageButtonCheck.setOnClickListener {
-            val number: Int = editText.text.toString().toInt()
-
-            if (number < random) {
-                hintTextView.text =  "Hint: The answer is higher than your guess!️"
-                editText.text.clear()
-
-            }else if (number > random){
-                hintTextView.text =  "Hint: The answer is lower than your guess!"
-                editText.text.clear()
-
-            }else {
-                hintTextView.text =  "Congratulations! You guessed correctly! "
-                editText.text.clear()
-
-            }
+            performCheck()
         }
-        imageButtontRefresh.setOnClickListener {
+        editText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                performCheck()
+                return@setOnEditorActionListener true
+            }
+            false
+        }
+        buttonPlayAgain.setOnClickListener {
             reset()
         }
     }
     fun reset(){
         random = nextInt(1,100)
+        count = 0
         hintTextView.text = ""
         textView.text = "Try to guess the number i'm thinking of from 1-100"
+        countTextView.text = "Guesses: $count"
         editText.text.clear()
     }
+    private fun performCheck() {
+        val number: Int = editText.text.toString().toInt()
+        count += 1
+        countTextView.text = "Guesses: $count"
+
+        if (number < random) {
+            hintTextView.text = "Hint: The answer is higher than your guess!️"
+            editText.text.clear()
+        } else if (number > random) {
+            hintTextView.text = "Hint: The answer is lower than your guess!"
+            editText.text.clear()
+        } else {
+            hintTextView.text = "Congratulations! You guessed correctly in $count times!"
+            editText.text.clear()
+        }
+    }
+
 }
